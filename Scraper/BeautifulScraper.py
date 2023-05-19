4d9b611bf3cf7c31a0b57696764cbc5933ca4641
allanarnesen <allan.arnesen@gmail.com>
2023-05-19 10:55:46 +0200

    .

---
+++ Scraper/BeautifulScraper.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+def finn_avisartikler_vg(overskrift, nettsted_url):
+    responce = requests.get(nettsted_url)
+    soup = BeautifulSoup(responce.text, 'html.parser')
+
+    artikler = []
+
+    divs = soup.find_all('div', class_='article-container')
+    for div in divs:
+        h2_element = div.find('h2')
+        lenke_element = div.find('a')
+        if h2_element is not None:
+            if overskrift.lower() in h2_element.text.lower():
+                artikkel_lenke = lenke_element['href']
+                artikkel_tittel = h2_element.text
+                artikkel = {'tittel':artikkel_tittel, 'lenke':artikkel_lenke}
+                artikler.append(artikkel)
+
+    return artikler
+
+
+overskrift = ''
+nettsted_url = ''
+
+funnede_artikler = finn_avisartikler_vg(overskrift, nettsted_url)
